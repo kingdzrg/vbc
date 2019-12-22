fb33827bba67c5c8caf369a238de419ec29d4b9b
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2019-12-22 18:19:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,146 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "kjn"
+
+
+
+
+client.on("message", message => {
+    if (message.content.startsWith("kjnobc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+
+  var prefix = "kjn";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+
+
+
+
+
+client.on('message', message => {
+    var  user = message.mentions.users.first() || message.author;
+if (message.content.startsWith("kjnavatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
+
+
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
+
+
+
+
+
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('kjnadminbc')){
+if(!message.author.id === '537675177677291530') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+  
+  client.on('message', msg => {
+    if(msg.content === 'kjnhelp')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
+  
+  
+  
+  
+  
+  client.on("message", message => {
+    if (message.content === "knjhelp") {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#00FF00")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`** HELP
+       kjnobc | لأرسال برود كاست للكل
+       kjnbc  |  لأرسال برود كاست للأونلاين
+       kjninvite | لدعوة البوت الي سيرفرك
+       kjnsupport | سيرفر السبورت** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+
+
+ 
+
+   client.on("message", message => {
+    if (message.content === "kjnsupport") {
+     const embed = new Discord.RichEmbed()
+         .setColor("RANDOM")
+         .addField('❤سيرفر الدعم الفني', ` https://discord.gg/Gd4eux `)
+     message.author.send({embed});
+   
+    }
+   });
+
+
+
+
+
+   client.on('message', message => {
+    if (message.content.startsWith("kjnbot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Broadcast Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+                  .addField('``My Prefix``' , `[nope]` , true)
+                  .addField('``My Language``' , `[ Java Script ]` , true)
+                    })
+}
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity("KiNG , Dz",{type: 'WATCHING'});
+  
+  });
+
+
+
+client.login(process.env.TOKEN);
